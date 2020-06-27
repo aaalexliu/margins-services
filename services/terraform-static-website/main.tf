a1f61a380bf9fdb81657be92ac12e95333191b2f
alexliusq <uilxela7@gmail.com>
2020-06-27 15:19:31 +0800

    created ACM certificate with subject alternative name that points to non-www root domain

--- services/terraform-static-website/main.tf
+++ services/terraform-static-website/main.tf
@@ -46,3 +46,16 @@ EOF
 #   content_type = "text/html"
 
 # }
+
+// Use the AWS Certificate Manager to create an SSL cert for our domain.
+// This resource won't be created until you receive the email verifying you
+// own the domain and you click on the confirmation link.
+resource "aws_acm_certificate" "certificate" {
+  // We want a wildcard cert so we can host subdomains later.
+  domain_name       = "*.${var.root_domain_name}"
+  validation_method = "DNS"
+
+  // We also want the cert to be valid for the root domain even though we'll be
+  // redirecting to the www. domain immediately.
+  subject_alternative_names = ["${var.root_domain_name}"]
+}
