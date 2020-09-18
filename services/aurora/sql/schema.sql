6cd7c8f2b33d832eec7b2e4b0d8a56acefb7d713
alexliusq <uilxela7@gmail.com>
2020-09-18 14:39:03 +0800

    changed user to account to avoid user reserved keyword

--- services/aurora/sql/schema.sql
+++ services/aurora/sql/schema.sql
@@ -1,10 +1,12 @@
-CREATE TABLE "publication" (
-  "publication_id" SERIAL PRIMARY KEY
-  "created_at" timestamp
+CREATE SCHEMA margins_private;
+
+CREATE TABLE margins_private.publication (
+  "publication_id" serial PRIMARY KEY,
+  "created_at" timestamp,
   "last_modified" timestamp DEFAULT now()
 );
 
-CREATE TABLE "book" (
+CREATE TABLE margins_private.book (
   "publication_id" int,
   "title" text NOT NULL,
   "isbn" char(13) UNIQUE NOT NULL,
@@ -16,8 +18,8 @@ CREATE TABLE "book" (
   "type" text
 );
 
-CREATE TABLE "annotation" (
-  "annotation_id" SERIAL PRIMARY KEY,
+CREATE TABLE margins_private.book (
+  "annotation_id" serial PRIMARY KEY,
   "publication_id" int,
   "location_begin" int,
   "location_end" int,
@@ -26,25 +28,24 @@ CREATE TABLE "annotation" (
   "highlight_color" text,
   "note" text,
   "statusline" text UNIQUE,
-  "page" int
-  "created_at" timestamp
+  "page" int "created_at" timestamp,
   "last_modified" timestamp DEFAULT now()
 );
 
-CREATE TABLE "author" (
-  "author_id" SERIAL PRIMARY KEY,
+CREATE TABLE margins_private.author (
+  "author_id" serial PRIMARY KEY,
   "first_name" text,
   "last_name" text
 );
 
-CREATE TABLE "publication_author" (
+CREATE TABLE margins_private.publication_author (
   "publication_id" int,
   "author_id" int,
   PRIMARY KEY ("publication_id", "author_id")
 );
 
-CREATE TABLE "user" (
-  "user_id" uuid PRIMARY KEY,
+CREATE TABLE margins_private.account (
+  "account_id" uuid PRIMARY KEY,
   "email" text,
   "last_modified" timestamp,
   "created_at" timestamp,
@@ -52,53 +53,65 @@ CREATE TABLE "user" (
   "email_verified" boolean
 );
 
-CREATE TABLE "user_publication" (
-  "user_id" uuid,
+CREATE TABLE "account_publication" (
+  "account_id" uuid,
   "publication_id" int,
-  PRIMARY KEY ("user_id", "publication_id")
+  PRIMARY KEY ("account_id", "publication_id")
 );
 
-CREATE TABLE "user_annotation" (
-  "user_id" uuid,
+CREATE TABLE "account_annotation" (
+  "account_id" uuid,
   "annotation_id" int,
-  PRIMARY KEY ("user_id", "annotation_id")
+  PRIMARY KEY ("account_id", "annotation_id")
 );
 
 CREATE TABLE "tag" (
-  "tag_id" SERIAL PRIMARY KEY,
+  "tag_id" serial PRIMARY KEY,
   "tag_name" text
 );
 
-CREATE TABLE "user_tag_annotation" (
-  "user_id" uuid,
+CREATE TABLE "account_tag_annotation" (
+  "account_id" uuid,
   "tag_id" int,
   "annotation_id" int,
-  PRIMARY KEY ("user_id", "tag_id", "annotation_id")
+  PRIMARY KEY ("account_id", "tag_id", "annotation_id")
 );
 
-ALTER TABLE "book" ADD FOREIGN KEY ("publication_id") REFERENCES "publication" ("publication_id");
+ALTER TABLE "book"
+  ADD FOREIGN KEY ("publication_id") REFERENCES "publication" ("publication_id");
 
-ALTER TABLE "annotation" ADD FOREIGN KEY ("publication_id") REFERENCES "publication" ("publication_id");
+ALTER TABLE "annotation"
+  ADD FOREIGN KEY ("publication_id") REFERENCES "publication" ("publication_id");
 
-ALTER TABLE "publication_author" ADD FOREIGN KEY ("publication_id") REFERENCES "publication" ("publication_id");
+ALTER TABLE "publication_author"
+  ADD FOREIGN KEY ("publication_id") REFERENCES "publication" ("publication_id");
 
-ALTER TABLE "publication_author" ADD FOREIGN KEY ("author_id") REFERENCES "author" ("author_id");
+ALTER TABLE "publication_author"
+  ADD FOREIGN KEY ("author_id") REFERENCES "author" ("author_id");
 
-ALTER TABLE "user_publication" ADD FOREIGN KEY ("user_id") REFERENCES "user" ("user_id");
+ALTER TABLE "account_publication"
+  ADD FOREIGN KEY ("account_id") REFERENCES "account" ("account_id");
 
-ALTER TABLE "user_publication" ADD FOREIGN KEY ("publication_id") REFERENCES "publication" ("publication_id");
+ALTER TABLE "account_publication"
+  ADD FOREIGN KEY ("publication_id") REFERENCES "publication" ("publication_id");
 
-ALTER TABLE "user_annotation" ADD FOREIGN KEY ("user_id") REFERENCES "user" ("user_id");
+ALTER TABLE "account_annotation"
+  ADD FOREIGN KEY ("account_id") REFERENCES "account" ("account_id");
 
-ALTER TABLE "user_annotation" ADD FOREIGN KEY ("annotation_id") REFERENCES "annotation" ("annotation_id");
+ALTER TABLE "account_annotation"
+  ADD FOREIGN KEY ("annotation_id") REFERENCES "annotation" ("annotation_id");
 
-ALTER TABLE "user_tag_annotation" ADD FOREIGN KEY ("user_id") REFERENCES "user" ("user_id");
+ALTER TABLE "account_tag_annotation"
+  ADD FOREIGN KEY ("account_id") REFERENCES "account" ("account_id");
 
-ALTER TABLE "user_tag_annotation" ADD FOREIGN KEY ("tag_id") REFERENCES "tag" ("tag_id");
+ALTER TABLE "account_tag_annotation"
+  ADD FOREIGN KEY ("tag_id") REFERENCES "tag" ("tag_id");
 
-ALTER TABLE "user_tag_annotation" ADD FOREIGN KEY ("annotation_id") REFERENCES "annotation" ("annotation_id");
+ALTER TABLE "account_tag_annotation"
+  ADD FOREIGN KEY ("annotation_id") REFERENCES "annotation" ("annotation_id");
 
-ALTER TABLE "user_tag_annotation" ADD FOREIGN KEY ("user_id") REFERENCES "user_tag_annotation" ("tag_id");
+ALTER TABLE "account_tag_annotation"
+  ADD FOREIGN KEY ("account_id") REFERENCES "account_tag_annotation" ("tag_id");
 
 CREATE INDEX ON "book" ("publication_id");
 
@@ -106,4 +119,8 @@ CREATE INDEX ON "annotation" ("publication_id");
 
 CREATE INDEX ON "publication_author" ("author_id");
 
-CREATE INDEX ON "user_tag_annotation" ("annotation_id", "tag_id");
+CREATE INDEX ON "account_tag_annotation" ("annotation_id", "tag_id");
+
+-- FUNCTIONS
+
+CREATE FUNCTION 
