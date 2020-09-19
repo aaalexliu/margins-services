471e1d3156349119bb99e53fb732daef982067a8
alexliusq <uilxela7@gmail.com>
2020-09-19 14:15:44 +0800

    set schema to margins_private in order to avoid repeating myself with fully qualified table names, added view where tags per annotaiton are denormalized into a json array

--- services/aurora/sql/schema.sql
+++ services/aurora/sql/schema.sql
@@ -1,10 +1,12 @@
 CREATE SCHEMA margins_private;
 
-CREATE TABLE margins_private.publication (
+SET SCHEMA 'margins_private';
+
+CREATE TABLE publication (
   "publication_id" serial PRIMARY KEY,
 );
 
-CREATE TABLE margins_private.book (
+CREATE TABLE book (
   "publication_id" int REFERENCES publication(publication_id),
   "title" text NOT NULL,
   "isbn" char(13) UNIQUE NOT NULL,
@@ -16,14 +18,14 @@ CREATE TABLE margins_private.book (
   "type" text
 );
 
-CREATE INDEX margins_private.book_publication_id_index ON margins_private.book(publication_id);
+CREATE INDEX book_publication_id_index ON book(publication_id);
 
-CREATE TABLE margins_private.annotation (
+CREATE TABLE annotation (
   "annotation_id" serial PRIMARY KEY,
   "publication_id" int REFERENCES publication(publication_id),
   "location_begin" int,
   "location_end" int,
-  "time" timestamp,
+  "recorded_at" timestamp,
   "highlight" text,
   "highlight_color" text,
   "note" text,
@@ -33,24 +35,24 @@ CREATE TABLE margins_private.annotation (
   "last_modified" timestamp DEFAULT now()
 );
 
-CREATE INDEX margins_private.annotation_publication_id_index ON margins_private.annotation(publication_id);
+CREATE INDEX annotation_publication_id_index ON annotation(publication_id);
 
-CREATE TABLE margins_private.author (
+CREATE TABLE author (
   "author_id" serial PRIMARY KEY,
   "first_name" text,
   "last_name" text
 );
 
-CREATE TABLE margins_private.publication_author (
+CREATE TABLE publication_author (
   "publication_id" int REFERENCES publication(publication_id),
   "author_id" int REFERENCES author(author_id),
   PRIMARY KEY ("publication_id", "author_id")
 );
 
-CREATE INDEX margins_private.publication_author_author_id_index ON margins_private.publication_author(author_id);
+CREATE INDEX publication_author_author_id_index ON publication_author(author_id);
 -- primary index order is publication_id first so to search author order doesn't match
 
-CREATE TABLE margins_private.account (
+CREATE TABLE account (
   "account_id" uuid PRIMARY KEY,
   "email" text,
   "last_modified" timestamp,
@@ -59,7 +61,7 @@ CREATE TABLE margins_private.account (
   "email_verified" boolean
 );
 
-CREATE TABLE margins_private.account_publication (
+CREATE TABLE account_publication (
   "account_id" uuid REFERENCES account(account_id),
   "publication_id" int REFERENCES publication(publication_id),
   "created_at" timestamp DEFAULT now(),
@@ -67,34 +69,43 @@ CREATE TABLE margins_private.account_publication (
   PRIMARY KEY ("account_id", "publication_id")
 );
 
-CREATE TABLE margins_private.account_annotation (
+CREATE TABLE account_annotation (
   "account_id" uuid REFERENCES account(account_id),
   "annotation_id" int REFERENCES annotation(annotation_id),
   PRIMARY KEY ("account_id", "annotation_id")
 );
 
-CREATE TABLE margins_private.tag (
+CREATE TABLE tag (
   "tag_id" serial PRIMARY KEY,
   "name" text
 );
 
-CREATE TABLE margins_private.account_tag_annotation (
-  "account_id" uuid REFERENCES account(account_id),
+CREATE TABLE annotation_tag (
+  "annnotation_id" int REFERENCES annotation(annotation_id),
   "tag_id" int REFERENCES tag(tag_id),
-  "annotation_id" int REFERENCES annotation(annotation_id),
-  PRIMARY KEY ("account_id", "tag_id", "annotation_id")
+  PRIMARY KEY ("annotation_id", "tag_id")
 );
 
-CREATE INDEX margins_private.account_tag_annotation_annotation_index ON margins_private.account_tag_annotation(annotation_id);
+CREATE INDEX account_tag_annotation_annotation_index ON account_tag_annotation(annotation_id);
 -- Primary key order is annotation_id last so to optimize for annotation_id create an index
 
 -- VIEWS
 
- 
+CREATE VIEW account_tag_annotation AS
+  SELECT a.account_id, a.annotation_id, b.tag_id FROM account_annotation AS a
+  JOIN annotation_tag AS b ON (a.annotation_id = b.annotation_id);
+
+CREATE VIEW full_annotation_tag AS
+  SELECT a.location_begin, a.location_end, a.recorded_at, a.highlight, a.highlight_color,
+  a.note, a.statusline, a.page, a.last_modified, json_agg(tag.name) AS all_tags FROM annotation AS a
+    INNER JOIN annotation_tag ON annotation.annotation_id = annotation_tag.annotation_id
+    INNER JOIN tag ON account_tag.tag_id = tag.tag_id
+  GROUP BY tag.tag_id;
+
 
 -- FUNCTIONS
 
-CREATE FUNCTION margins_private.account_full_name(account margins_private.account) returns text as $$
+CREATE FUNCTION account_full_name(account account) returns text as $$
   SELECT account.first_name || ' ' || account.last_name
   -- SELECT concat(account.first_name, ' ', account.last_name)
 $$ language sql stable;
