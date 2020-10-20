106460186655f2c447d63edfb01983c4677c07fa
alexliusq <uilxela7@gmail.com>
2020-10-20 09:10:03 +0800

    basically the tutorial from postgraphile has a function that downloads the jwks and gets the pem from them already, edited for cognito

--- services/aurora/testing/graphql/src/server.js
+++ services/aurora/testing/graphql/src/server.js
@@ -3,10 +3,39 @@ const { postgraphile } = require("postgraphile");
 const PgManyToManyPlugin = require("@graphile-contrib/pg-many-to-many");
 const PostGraphileNestedMutations = require('postgraphile-plugin-nested-mutations');
 
+const jwt = require("express-jwt");
+const jwksRsa = require("jwks-rsa");
+const ISSUER_DOMAIN = `https://cognito-idp.${process.env.COGNITO_REGION}.amazonaws.com/${process.env.COGNITO_USER_POOL_ID}`;
+
+// Authentication middleware. When used, the
+// Access Token must exist and be verified against
+// the Auth0 JSON Web Key Set.
+// On successful verification, the payload of the
+// decrypted Access Token is appended to the
+// request (`req`) as a `user` parameter.
+const checkJwt = jwt({
+  // Dynamically provide a signing key
+  // based on the `kid` in the header and
+  // the signing keys provided by the JWKS endpoint.
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `${ISSUER_DOMAIN}/.well-known/jwks.json`,
+  }),
+
+  // Validate the audience and the issuer.
+  audience: "4v1q302r040fct1ve3kkhoo30b",
+  issuer: ISSUER_DOMAIN,
+  algorithms: ["RS256"],
+});
+
 const app = express();
 
+app.use('/graphql', checkJwt);
+
 app.use(
-  postgraphile(process.env.OWNER_URL, "margins_public", {
+  postgraphile(process.env.DATABASE_URL, "margins_public", {
     watchPg: true,
     graphiql: true,
     enhanceGraphiql: true,
