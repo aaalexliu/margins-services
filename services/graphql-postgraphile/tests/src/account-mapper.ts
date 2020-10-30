3b3702339d7b4594c733e2998c47aefe458de950
alexliusq <uilxela7@gmail.com>
2020-10-30 21:40:31 +0800

    built out account mapper to support cognito id token objects

--- services/graphql-postgraphile/tests/src/account-mapper.ts
+++ services/graphql-postgraphile/tests/src/account-mapper.ts
@@ -29,8 +29,13 @@ interface AccountInputVar {
 interface Account {
   accountId: string,
   email: string,
-  emailVerified: boolean,
-  fullName: string
+  emailVerified: boolean
+}
+
+interface CognitoAccount {
+  sub: string,
+  email: string,
+  emailVerified: boolean
 }
 
 export default class AccountMapper {
@@ -44,10 +49,21 @@ export default class AccountMapper {
     });
   }
 
+  async createAccountFromCognito(cognitoAccount: CognitoAccount) {
+    const account = {
+      accountId: cognitoAccount.sub,
+      email: cognitoAccount.email,
+      emailVerified: cognitoAccount.emailVerified
+    }
+    const response = await this.createAccount(account);
+  }
+
   async createAccount(account: Account) {
     const accountInputVar = this.createAccountInput(account);
     const response = await this.graphQLClient.request(CREATE_ACCOUNT, accountInputVar);
     console.log(response);
+
+    return response;
   }
 
   createAccountInput(account: Account): AccountInputVar {
@@ -60,3 +76,24 @@ export default class AccountMapper {
     }
   }
 }
+
+//sample id token from cognito
+// {
+//   "sub": "46d3f3d1-879b-4314-9301-ae470c5a2062",
+//   "cognito:groups": [
+//     "margins_account"
+//   ],
+//   "email_verified": true,
+//   "iss": "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_jcGG3tVBb",
+//   "cognito:username": "46d3f3d1-879b-4314-9301-ae470c5a2062",
+//   "cognito:roles": [
+//     "arn:aws:iam::516851544810:role/amplify-marginsmereact-dev-205104-authRole"
+//   ],
+//   "aud": "4v1q302r040fct1ve3kkhoo30b",
+//   "event_id": "d207a8f6-f162-426c-acf8-890644f8b58e",
+//   "token_use": "id",
+//   "auth_time": 1604060976,
+//   "exp": 1604147376,
+//   "iat": 1604060976,
+//   "email": "uilxela7@gmail.com"
+// }
