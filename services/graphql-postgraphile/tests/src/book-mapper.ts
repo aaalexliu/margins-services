8b593bb886cc8f799757cbeaac284899670b96b4
alexliusq <uilxela7@gmail.com>
2020-10-31 23:21:01 +0800

    halfway through building out full find and create for author and books. this is a crap ton of code for just a few simple tasks. query after mutation after query. should probably figure out how to optimize this

--- services/graphql-postgraphile/tests/src/book-mapper.ts
+++ services/graphql-postgraphile/tests/src/book-mapper.ts
@@ -4,7 +4,6 @@ import { ObjectID } from 'mongodb';
 import {
   CreatePublicationInput,
   CreateAuthorInput,
-  CreateBookInput
 } from '../../__generated__/types';
 
 
@@ -47,6 +46,50 @@ const CREATE_BOOK = gql`
   }
 `;
 
+const GET_PUBLICATION = gql`
+  query GetPublicationByAccountAndTitle($accountId: UUID!, $title: String!) {
+    __typename
+    publicationByAccountIdAndTitle(accountId: $accountId, title: $title) {
+      publicationId
+      title
+      accountId
+      additionalMeta
+    }
+  }
+`;
+
+const GET_AUTHOR = gql`
+  query GetAuthorByFullName($fullName: String!) {
+    __typename
+    authorByFullName(name: $fullName) {
+      authorId
+      name
+    }
+  }
+`;
+
+const CONNECT_AUTHOR = gql`
+  mutation ConnectAuthorToPublication($authorAndPublication: CreatePublicationAuthorInput!) {
+    __typename
+    createPublicationAuthor(input: {publicationAuthor: $authorAndPublication) {
+      publicationAuthor {
+        authorId
+        publicationId
+      }
+    }
+  }
+`;
+
+
+interface GetPublicationVar {
+  accountId: string,
+  title: string
+}
+
+interface GetAuthorVar {
+  fullName: string
+}
+
 interface AuthorInputVar {
   authorInput: CreateAuthorInput
 }
@@ -61,6 +104,11 @@ interface Book {
   isbn13?: string,
   description?: string,
   publisher?: string,
+}
+
+interface BookResponse {
+  publicationId: string,
+  title?: string
 }
 
 export default class BookMapper {
@@ -81,22 +129,17 @@ export default class BookMapper {
     return objectId.toHexString();
   }
 
-  async findOrCreateBook(book: Book) {
-
-  }
-
-  async createBook(book: Book) {
-    const bookMutationVars = this.createBookInput(book);
-    try {
-      const bookResponse = await this.graphQLClient.request(CREATE_BOOK, bookMutationVars);
-      console.log(bookResponse);
-    } catch(error) {
-      console.error(JSON.stringify(error, null, 2))
+  async findOrCreateBook(book: Book): BookResponse {
+    let publicationId;
+    let bookResponse = await this.findBookByTitle(book);
+    if (!bookResponse) {
+      bookResponse = await this.createBook(book);
     }
+    // find or create authors if successful
 
-    // create authors if successful
     const authors = book.authors;
-    const bookPublicationId = this.getBookPublicationId(bookMutationVars);
+    publicationId = bookResponse.publicationId;
+    // const bookPublicationId = this.getBookPublicationId(bookMutationVars);
     const authorResponses = await Promise.all(authors.map((author) => {
         const authorMutationVars = this.createAuthorInput(author, bookPublicationId);
         return this.graphQLClient.request(CREATE_AUTHOR, authorMutationVars);
@@ -105,29 +148,54 @@ export default class BookMapper {
     console.log(authorResponses);
   }
 
+  async findBookByTitle(book: Book): Promise<BookResponse> {
+    const title = book.title;
+    const GetPublicationVar = this.createGetPublicationVar(title);
+    const response = await this.graphQLClient.request(GET_PUBLICATION, GetPublicationVar);
+    console.log('get publication response', response);
+    return response.data.publication;
+  }
+
+  createGetPublicationVar(title: string): GetPublicationVar {
+    return {
+      title,
+      accountId: this.accountId
+    }
+  }
+
+  async createBook(book: Book) {
+    const bookMutationVars = this.createBookInput(book);
+    try {
+      const bookResponse = await this.graphQLClient.request(CREATE_BOOK, bookMutationVars);
+      console.log('create book response', bookResponse);
+      return bookResponse.publication;
+    } catch(error) {
+      console.error(JSON.stringify(error, null, 2))
+    }
+  }
+
   createBookInput(book: Book): BookInputVar {
     const publicationId = this.generateObjectId();
     // authors willl be added in separate mutation
+
+    // right now no need for all the book specific info - don't have it anyway in most cases
+    // in the future will make sure to fully populate book with info from google books api
+    // that way all the info will be valid and hopefully not conflict
     const bookNoAuthors = Object.assign({}, book);
     delete bookNoAuthors.authors;
     return {
       bookInput: {
         publication: {
           publicationId,
-          bookUsingPublicationId: {
-            create: [
-              {
-              ...bookNoAuthors
-              }
-            ]
-          },
-          accountPublicationsUsingPublicationId: {
-            create: [
-              {
-                accountId: this.accountId
-              }
-            ]
-          }
+          accountId: this.accountId,
+          title: book.title,
+          // bookUsingPublicationId: {
+          //   create: [
+          //     {
+          //     ...bookNoAuthors
+          //     }
+          //   ]
+          // }
         }
       }
     }
@@ -137,10 +205,27 @@ export default class BookMapper {
     return input.bookInput.publication.publicationId;
   }
 
+  async findOrCreateAuthor(name: string, publicationId: string) {
+    let author = await this.findAuthor(name);
+    if (author) {
+
+    }
+  }
+
+  async connectAuthorAndPublication(authorId: string, publicationId) {
+
+  }
+
+  async findAuthor(name: string) {
+    const getAuthorVar: GetAuthorVar = { fullName: name};
+    const authorResponse = await this.graphQLClient.request(GET_AUTHOR, getAuthorVar);
+    return authorResponse.author;
+  }
+
   async createAuthor(name: string, publicationId: string) {
-    const mutationVar = this.createAuthorInput(name, publicationId);
-    const res = await this.graphQLClient.request(CREATE_AUTHOR, mutationVar);
-    return res;
+    const createAuthorVar = this.createAuthorInput(name, publicationId);
+    const authorResponse = await this.graphQLClient.request(CREATE_AUTHOR, createAuthorVar);
+    return authorResponse.author;
   }
 
   createAuthorInput(name: string, publicationId: string): AuthorInputVar {
