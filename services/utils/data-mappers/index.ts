b610385d3fd0d808e067eb64d0ecf9c3726956c3
alexliusq <uilxela7@gmail.com>
2020-11-02 13:46:17 +0800

    tsconfig and index.ts to export all the mapper classes

---
+++ services/utils/data-mappers/index.ts
@@ -0,0 +1,3 @@
+export { default as AccountMapper } from './src/account-mapper';
+export { default as BookMapper } from './src/book-mapper';
+export { default as AnnotationMapper } from './src/annotation-mapper';
