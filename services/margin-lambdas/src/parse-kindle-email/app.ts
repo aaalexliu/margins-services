2a92d5a27f0bf73dde0b63cab1ea3a1a5152e699
alexliusq <uilxela7@gmail.com>
2020-11-02 22:25:27 +0800

    added kindleconvert, got simplemailparser working, added test files

--- services/margin-lambdas/src/parse-kindle-email/app.ts
+++ services/margin-lambdas/src/parse-kindle-email/app.ts
@@ -2,6 +2,7 @@
 import AWS from'aws-sdk';
 import * as util from 'util';
 import { simpleParser } from 'mailparser';
+import { KindleConverter } from '../../../utils/kindle-html-converter/';
 // const sharp = require('sharp');
 
 // get reference to S3 client
@@ -12,6 +13,8 @@ exports.lambdaHandler = async (event, context, callback) => {
     // Read options from the event parameter.
     console.log("Reading options from event:\n", util.inspect(event, {depth: 5}));
     const srcBucket = event.Records[0].s3.bucket.name;
+    console.log('s3 event record');
+    console.log(event.Records[0].s3)
     // Object key may have spaces or unicode non-ASCII characters.
     const srcKey    = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));
     const dstBucket = srcBucket;
@@ -38,35 +41,44 @@ exports.lambdaHandler = async (event, context, callback) => {
             Bucket: srcBucket,
             Key: srcKey
         };
-        email = await s3.getObject(params).promise();
+        const emailResponse = await s3.getObject(params).promise();
+        email = emailResponse.Body;
 
     } catch (error) {
         console.log(error);
         return;
     }
+    console.log(email);
 
     const parsedMail = await simpleParser(email);
     const attachment = getAttachment(parsedMail);
-    
+    // console.log(attachment);
+
+    const kindleConverter = new KindleConverter(attachment);
+    const bookInfo = kindleConverter.getBookInfo();
+    console.log('book info', bookInfo);
+
+    const bookNotes = kindleConverter.getBookNotes();
+    console.log('book notes', bookNotes);
 
     // Upload the thumbnail image to the destination bucket
-    try {
-        const destparams = {
-            Bucket: dstBucket,
-            Key: dstKey,
-            Body: buffer,
-            ContentType: "application/json; charset=utf-8"
-        };
+    // try {
+    //     const destparams = {
+    //         Bucket: dstBucket,
+    //         Key: dstKey,
+    //         Body: buffer,
+    //         ContentType: "application/json; charset=utf-8"
+    //     };
 
-        const putResult = await s3.putObject(destparams).promise(); 
+    //     const putResult = await s3.putObject(destparams).promise(); 
         
-    } catch (error) {
-        console.log(error);
-        return;
-    } 
+    // } catch (error) {
+    //     console.log(error);
+    //     return;
+    // } 
         
-    console.log('Successfully resized ' + srcBucket + '/' + srcKey +
-        ' and uploaded to ' + dstBucket + '/' + dstKey); 
+    // console.log('Successfully resized ' + srcBucket + '/' + srcKey +
+    //     ' and uploaded to ' + dstBucket + '/' + dstKey); 
 };
 
 function getAttachment(mail) {
